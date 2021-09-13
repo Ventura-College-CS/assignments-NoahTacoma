098a9ec1191cbca5e7b8d16037517f6fdcd828a4
Noah Tacoma <halo123329@hotmail.com>
2021-09-13 14:13:39 -0700

    3

---
+++ array3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// string mainstr="chocolate";
+    // string substr="col";
+
+    char mainstr[10]="chocolate";
+    char substr[10]="col";
+    int pos;
+
+    // pos = mainstr.find(substr) ;
+    cout << sizeof(mainstr) <<endl;
+    cout << sizeof(str) << endl;
+
+
+    //cout << " found at position " << pos << endl;
+
+}

39527753b46851a7e18236eefb0a60aad1243c9b
Noah Tacoma <halo123329@hotmail.com>
2021-09-20 14:40:04 -0700

    9/20 Assignment 1-1 .1

---
+++ Assignment1-1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct Student {
+  int id;
+  char sname[20];
+  double score[2];
+  double sum;
+  double avg;
+};
+
+int main()
+{
+    ifstream ifs;
+    ofstream ofs;
+
+    Student s;
+
+    ifs.open("students.txt");
+    ofs.open("students.bin");
+
+
+    ifs >> s.id ;
+    ifs >> s.sname ;
+    ifs >> s.score[0] ;
+    ifs >> s.score[1] ;
+    s.sum = s.score[0] + s.score[1];
+    s.avg = s.sum / 2.0;
+    cout << " ID : " << s.id << endl;
+    cout << " Name : " << s.sname << endl;
+    cout << " Score 1 : " << s.score[0] << endl;
+    cout << " Score 2 : " << s.score[1] << endl;
+    cout << " Sum : " << s.sum << endl;
+    cout << " Avg : " << s.avg << endl;
+
+}

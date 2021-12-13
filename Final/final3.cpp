013f71dbfb295fb8e87af296c0bd706c3472dbe3
Noah Tacoma <halo123329@hotmail.com>
2021-12-13 01:59:58 -0800

    final3 done

--- Final/final3.cpp
+++ Final/final3.cpp
@@ -1,34 +1,52 @@
 #include <iostream>
+#include <queue>
 using namespace std;
+   
 
+    int stack_pop(vector<int>* stack){
+		int a = stack->back();
+		stack->pop_back();
+		return a;
+    }
 
-int postfix_eval(char* array, int size){
-    for(int i = 0; i < size; i++){
-        int v1 = stack.pop();
-        int v2 = stack.pop();
-        if(array[i] == '*'){
-            stack.push(v1*v2);
+int postfix_eval(string input){
+    vector<int> stack;
+    for(int i = 0; i < input.size(); i++){
+        if(input[i] == '*'){
+            int v1 = stack_pop(&stack);
+            int v2 = stack_pop(&stack);
+            stack.push_back(v1*v2);
         }
-        else if(array[i] == '/'){
-            stack.push(v1/v2);
+        else if(input[i] == '/'){
+            int v1 = stack_pop(&stack);
+            int v2 = stack_pop(&stack);
+            stack.push_back(v2/v1);
         }
-        else if(array[i] == '+'){
-            stack.push(v1+v2);
+        else if(input[i] == '+'){
+            int v1 = stack_pop(&stack);
+            int v2 = stack_pop(&stack);
+            stack.push_back(v1+v2);
         }
-        else if(array[i] == '-'){
-            stack.push(v1-v2);
+        else if(input[i] == '-'){
+            int v1 = stack_pop(&stack);
+            int v2 = stack_pop(&stack);
+            stack.push_back(v2-v1);
         }
         else{
-            state = 1;
-            stack.push(array[i]);
+            stack.push_back(input[i]-'0');
         }
     }
+    return(stack_pop(&stack));
 }
 
 
 
 int main(){
-    
+    string input;
+    cout << "Input postfix expression :";
+    cin >> input;
+    cout << postfix_eval(input);
+
 
 
 }

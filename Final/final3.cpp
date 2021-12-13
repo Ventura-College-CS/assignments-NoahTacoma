c7957049026951c160f5f7c4ce7989e1f003fb8e
Noah Tacoma <halo123329@hotmail.com>
2021-12-13 01:05:48 -0800

    1

---
+++ Final/final3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+int postfix_eval(char* array, int size){
+    int result;
+    int v1;
+    int v2;
+    for(int i = 0; i < size; i++){
+        if(array[i] == '*'){
+
+        }
+        else if(array[i] == '/'){
+
+        }
+         else if(array[i] == '+'){
+            
+        }
+        else if(array[i] == '-'){
+            
+        }
+        
+    }
+}
+
+
+
+int main(){
+    
+
+
+}

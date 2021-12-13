786a49ec4a396b91db2d31cfbdb5eb51159ee13b
Noah Tacoma <halo123329@hotmail.com>
2021-12-12 19:12:01 -0800

    first

---
+++ Final/final1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+
+class course
+{
+    public:
+    int ID;
+    string name;
+    int credit;
+    //Constructor and Member functions that would be necessary to be used in this question
+}
+// In this program, we should manage 10 course objects.
+// To set up 10 object values, you can use the text file from input data.
+// Create the binary search function to find a certain course with the course ID.
+// Before calling this function, the course objects should be sorted based on the ID value.
+// Implement the full source program and show the results with the various test data.

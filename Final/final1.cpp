a1a23a67a6568d18b91d769244959d4d36ee8271
Noah Tacoma <halo123329@hotmail.com>
2021-12-12 20:33:48 -0800

    temparray

--- Final/final1.cpp
+++ Final/final1.cpp
@@ -10,21 +10,44 @@ public:
     int credit;
     string name;
 
-
-     course(int cID, int ccredit, string cname)
-     {
-         ID = cID;
-         credit = ccredit;
-         name = cname;
-     }
+    course() : ID(0), name() {}
+    course(int cID, int ccredit, string cname)
+    {
+        ID = cID;
+        credit = ccredit;
+        name = cname;
+    }
 
     //Constructor and Member functions that would be necessary to be used in this question
 };
 
-int main()
+int main() 
 {
-    course c1(101,90,"john");
-    cout << c1.ID << "\n";
+    course c1(101,90,"john"); //I could not find text file for input data.
+    course c2(102,80,"john");
+    course c3(103,70,"john");
+    course c4(104,60,"john");
+    course c5(105,50,"john");
+    course c6(106,40,"john");
+    course c7(107,30,"john");
+    course c8(108,20,"john");
+    course c9(109,10,"john");
+    course c10(110,100,"john");
+
+    course carray[10];
+
+    carray[0] = c1;
+    carray[1] = c2;
+    carray[2] = c3;
+    carray[3] = c4;
+    carray[4] = c5;
+    carray[5] = c6;
+    carray[6] = c7;
+    carray[7] = c8;
+    carray[8] = c9;
+    carray[9] = c10;
+
+//    cout << c1.ID << "\n";
 }
 // In this program, we should manage 10 course objects.
 // To set up 10 object values, you can use the text file from input data.
